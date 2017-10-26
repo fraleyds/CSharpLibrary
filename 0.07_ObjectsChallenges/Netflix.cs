ed8992f37042d46fff8b47f24b1a1c3068cbf995
David Fraley <fraleyds@gmail.com>
2017-10-26 11:05:58 -0400

    added objects challenges

---
+++ 0.07_ObjectsChallenges/Netflix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_ObjectsChallenges
+{
+    class Netflix
+    {
+        // Properties
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+        // Methods
+        public void GetSuggestion()
+        {
+            if (this.Rating < 3 && this.Rating >=1)
+            {
+                Console.WriteLine("It's a tremendous waste of your time. Only watch if you're drinking lots of alcohol with friends.");
+            }
+            else if (this.Rating >= 3 && this.Rating < 4)
+            {
+                Console.WriteLine("If you don't have anything else on your plate, maybe go watch it. It ain't bad.");
+            }
+            else if (this.Rating >= 4 && this.Rating <= 5)
+            {
+                Console.WriteLine("Duuuuuude. Go watch it.");
+            }
+            else
+            {
+                Console.WriteLine("That's not an input I understand.");
+            }
+        }
+
+
+    }
+}
---
+++ 0.07_ObjectsChallenges/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_ObjectsChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Netflix strangerThings = new Netflix();
+            Netflix parksAndRecreation = new Netflix();
+            Netflix theOffice = new Netflix();
+
+            strangerThings.Name = "Stranger Things";
+            strangerThings.Genre = "Horror(?)";
+            strangerThings.Rating = 3.0;
+
+            parksAndRecreation.Name = "Parks & Recreation";
+            parksAndRecreation.Genre = "Comedy";
+            parksAndRecreation.Rating = 5.0;
+
+            theOffice.Name = "The Office";
+            theOffice.Genre = "Comedy";
+            theOffice.Rating = 3.8;
+
+            //strangerThings.GetSuggestion();
+            //parksAndRecreation.GetSuggestion();
+            //theOffice.GetSuggestion();
+
+            // Creating a list of Objects
+            List<Netflix> favoriteShows = new List<Netflix>();
+
+            favoriteShows.Add(theOffice);
+            favoriteShows.Add(strangerThings);
+            favoriteShows.Add(parksAndRecreation);
+
+            foreach (Netflix show in favoriteShows)
+            {
+                Console.WriteLine(show.Name);
+                Console.WriteLine(show.Genre);
+                Console.WriteLine(show.Rating);
+            }
+
+
+        }
+    }
+}
