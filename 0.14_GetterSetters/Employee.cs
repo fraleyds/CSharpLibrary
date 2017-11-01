3c5ea2e2905a657fbc39e8572d4f86fbce4cae9b
David Fraley <fraleyds@gmail.com>
2017-11-01 12:09:50 -0400

    added getters setters exercises

---
+++ 0.14_GetterSetters/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._14_GetterSetters
+{
+    class Employee
+    {
+        //public int ID { get; set; }
+        //public string Name { get; set; }
+
+        //Fields
+        private int _id;
+        private string _name;
+        private string _workQuote;
+
+        //Properties
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("The employee ID is invalid");
+                _id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                    throw new Exception("No name provided");
+                _name = value.Trim();
+            }
+        }
+
+        //Methods
+        public void SetWorkQuote(string quote)
+        {
+            this._workQuote = quote;
+        }
+
+        public string GetQuote()
+        {
+            return this._workQuote;
+        }
+
+    }
+}
---
+++ 0.14_GetterSetters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._14_GetterSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee newEmployee = new Employee();
+
+            Console.WriteLine("Enter Employee Name: ");
+            newEmployee.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee ID: ");
+            newEmployee.ID = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"New Employee: {newEmployee.Name} {newEmployee.ID}");
+
+            //Set work quote
+            Console.WriteLine($"Enter a common quote for {newEmployee.Name}");
+            newEmployee.SetWorkQuote(Console.ReadLine());
+
+            //Get work quote
+            Console.WriteLine(newEmployee.GetQuote());
+        }
+    }
+}
