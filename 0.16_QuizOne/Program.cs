ce06dee541f3f4c5be29aa19d3a06f845d52e112
David Fraley <fraleyds@gmail.com>
2017-11-02 10:21:45 -0400

    more quiz 1

--- 0.16_QuizOne/Program.cs
+++ 0.16_QuizOne/Program.cs
@@ -10,20 +10,40 @@ namespace _0._16_QuizOne
     {
         static void Main(string[] args)
         {
+            //Problem 1
             Triangle triangleOne = new Triangle(2, 4);
-            Console.WriteLine($"Area = {triangleOne.Area()}");
-
-            Triangle triangleTwo = new Triangle(3, 1);
-            Console.WriteLine($"Area = {triangleTwo.Area()}");
-
+            Triangle triangleTwo = new Triangle(3, 2);
             Triangle triangleThree = new Triangle(20, 4);
-            Console.WriteLine($"Area = {triangleThree.Area()}");
-
             Triangle triangleFour = new Triangle(900, 67);
-            Console.WriteLine($"Area = {triangleFour.Area()}");
-
             Triangle triangleFive = new Triangle(78, 52);
-            Console.WriteLine($"Area = {triangleFive.Area()}");
+
+            triangleOne.Area();
+            triangleTwo.Area();
+            triangleThree.Area();
+            triangleFour.Area();
+            triangleFive.Area();
+
+            //Problem 2
+            IEnumerable<Triangle> triangles = new List<Triangle>()
+            {
+                new Triangle {TBase=3,THeight=2,TName="Triangle 1"},
+                new Triangle {TBase=5,THeight=5,TName="Triangle 2"},
+                new Triangle {TBase=1,THeight=3,TName="Triangle 3"},
+                new Triangle {TBase=6,THeight=15,TName = "Triangle 4"},
+                new Triangle {TBase=8,THeight=3,TName="Triangle 5"},
+                new Triangle {TBase=90,THeight=123, TName="Triangle 6"},
+                new Triangle {TBase=6,THeight=6,TName="Triangle 7"},
+            };
+
+            IEnumerable<Triangle> query = from t in triangles
+                                          where t.Area() > 10
+                                          orderby t.TArea
+                                          select t;
+
+            foreach (Triangle triangle in query)
+            {
+                Console.WriteLine(triangle.TName);
+            }
         }
     }
 }
--- 0.16_QuizOne/Triangle.cs
+++ 0.16_QuizOne/Triangle.cs
@@ -14,15 +14,23 @@ namespace _0._16_QuizOne
             this.TBase = tBase;
             this.THeight = tHeight;
         }
+        public Triangle()
+        {
+
+        }
 
         //Properties
         public int TBase { get; set; }
         public int THeight { get; set; }
+        public double TArea { get; set; }
+        public string TName { get; set; }
 
         //Methods
         public override double Area()
         {
-            return ((TBase * THeight) / 2);    
+            TArea = ((TBase * THeight) / 2);
+            Console.WriteLine(TArea);
+            return TArea;
         }
 
     }
